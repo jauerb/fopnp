007df3281b5552f013763ab4eef9e37810f97f1d
Josh Auerbach <joshua.auerbach@epfl.ch>
2019-02-05 09:13:12 -0500

    fixed api_util.py to remove trailing newline on unix systems

--- py3/chapter01/api_util.py
+++ py3/chapter01/api_util.py
@@ -4,4 +4,4 @@
 def load_key(filename="google.key"):
     """Load API key from the specified filename."""
     with open("google.key") as f:
-        return f.read()
+        return f.read().strip()
